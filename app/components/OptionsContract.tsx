ab6821256ec87e9c019858ba7231638c459af614
alexm <alexmazur6222@gmail.com>
2024-10-05 02:54:01 -0600

    move to appshell, remove a lot of default css and such

--- app/components/OptionsContract.tsx
+++ app/components/OptionsContract.tsx
@@ -6,7 +6,7 @@ import { optionsContract } from "../actions/polygon";
 import { TextInput } from "@mantine/core";
 
 export const OptionsContract = React.memo(function _optionsContract() {
-  const [ticker, setTicker] = useState("SPY")
+  const [ticker, setTicker] = useState("")
 
   const [options, setOptions] = useState("");
   useEffect(() => {
@@ -19,7 +19,12 @@ export const OptionsContract = React.memo(function _optionsContract() {
   }, [setOptions]);
   return (
     <div>
-      <TextInput value={ticker} onChange={(event) => setTicker(event.currentTarget.value)} />
+      <TextInput
+        value={ticker}
+        onChange={(event) => setTicker(event.currentTarget.value)}
+        label="Ticker"
+        placeholder="AAPL"
+      />
       {options}
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
-import styles from "./page.module.css";
+'use client'
+import { AppShell  } from "@mantine/core";
 import { OptionsContract } from "./components/OptionsContract";
+import { useDisclosure } from "@mantine/hooks";
+import Image from 'next/image';
+import styles from "./page.module.scss";
 
 
 export default function Home() {
+  const [opened, { toggle }] = useDisclosure();
   return (
-    <div className={styles.page}>
-      <OptionsContract />
-    </div>
+    <AppShell
+      header={{height: 60}}
+      padding='md'
+    >
+      <AppShell.Header className={styles.header}>
+        <Image src={"/logo.webp"} alt={""} width="40" height="40"/>
+        Recourse
+      </AppShell.Header>
+      <AppShell.Main>
+        <OptionsContract />
+      </AppShell.Main>
+    </AppShell>
   );
 }
